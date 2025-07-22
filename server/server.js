a2ebb6bad70f57065b5a4a7058b84bcd8e4fd724
Mukesh1765 <161288818+Mukesh1765@users.noreply.github.com>
2025-07-22 15:39:48 +0530

    initial commit

---
+++ server/server.js
@@ -0,0 +1,55 @@
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+import http from "http"
+import { connectDB } from "./library/db.js"
+import userRouter from "./routes/userRoutes.js"
+import messageRouter from "./routes/messageRoutes.js"
+import { Server } from "socket.io"
+import cookieParser from "cookie-parser"
+
+const app = express()
+const server = http.createServer(app)
+
+export const io = new Server(server, {
+    cors: {
+        origin: ["http://localhost:5173"],
+        credentials: true,
+    }
+})
+
+export const userSocketMap = {}
+
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId
+    console.log("user connected:", userId)
+
+    if (userId) userSocketMap[userId] = socket.id
+
+    io.emit("getOnlineUsers", Object.keys(userSocketMap))
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected:", userId)
+        delete userSocketMap[userId]
+        io.emit("getOnlineUsers", Object.keys(userSocketMap))
+    })
+})
+
+app.use(cors())
+app.use(express.json({limit: "4mb"}))
+app.use(cookieParser())
+
+app.use("/api/status", (req, res) => res.send("Server is live now...!"))
+app.use("/api/auth", userRouter)
+app.use("/api/messages", messageRouter)
+
+await connectDB();
+
+const port = process.env.PORT || 3000
+if (process.env.NODE_ENV !== "production") {
+    server.listen(port, () => {
+        console.log("Server running on port", port)
+    })
+}
+
+export default server
